cbc6b247a308fbc1d63b06504cb0f6d8c51ac279
Ismael Alves <ismael.a.eugenio@gmail.com>
2025-04-16 12:52:53 -0300

    teste

---
+++ src/main/java/dev/java/pj/client/Controller.java
@@ -0,0 +1,19 @@
+package dev.java.pj.client;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController 
+@RequestMapping 
+public class Controller {
+	
+	@GetMapping("/boasVIndas")
+	public String boasVindas() {
+		
+		return "Essas é minha primeira menssagem";
+	}
+	
+	
+
+}
